a648bcb7015eec1b69970acd50b957f7d5a5ae03
Yekta83 <yektafatemeh931@gmail.com>
2024-12-11 00:36:09 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 //Circle myCircle = new Circle(5);
 //double area = myCircle.CalculateArea();
 //Console.WriteLine("area of circle: " + area);
+
+//class Program
+//{
+   //static void Main(string[] args)
+   // {
+      //  ProductPriceChecker checker = new ProductPriceChecker();
+       // checker.CheckPrices();
+  //  }
+//}
+
 class Program
 {
     static void Main(string[] args)
     {
-        ProductPriceChecker checker = new ProductPriceChecker();
-        checker.CheckPrices();
+        int[] myNumbers = { 5, 3, 1, 4, 2 };
+        MyArray myArray = new MyArray(myNumbers);
+        myArray.Print();
     }
 }
